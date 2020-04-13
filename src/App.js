4624811783e9ebc8fd702b1e3f1f85819187c029
Marin Virdol <m.virdol@gmail.com>
2020-04-13 10:48:00 +0200

    hot reload a react app in dev with react-hot-loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {hot} from 'react-hot-loader'
 
 class App extends React.Component {
   state = {
@@ -9,7 +10,9 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello World!!!</h1>
-        <h2>Count: {this.state.count}</h2>
+        <h2 className={this.state.count > 10 ? 'warning' : null}>
+          Count: {this.state.count}
+        </h2>
         <button
           onClick={() => this.setState((state) => ({count: state.count + 1}))}
         >
@@ -25,4 +28,4 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default hot(module)(App)
